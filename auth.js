3fd4bc27cb57f4e1322e2a7caadb29a3bc10f282
anderson barros <anderson.barros@ifpi.edu.br>
2023-03-09 14:30:19 -0300

    Novo commit feito em 09/03/2023 às 19h00m

---
+++ auth.js
@@ -0,0 +1,11 @@
+var auth = false;
+var user = "admin";
+var pass = "12345678";
+
+alert('digite o Usuario:');
+alert('digite a senha:');
+
+if auth:
+	alert('Usuario ou senha incorretos');
+else:
+	alert('Acesso autorizado');
